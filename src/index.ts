f88631811c9a8c3a30c1d24c9fb778c912be0e96
leon-do <leondo123@gmail.com>
2021-12-03 23:29:27 -0500

    socketio

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,10 @@ import { WebSocket } from "ws";
 
 const wss = new WebSocket.Server({ port: 3000 });
 
-interface Response {
-  id: string;
-  receipt: string;
-}
-
 // f. Server Socket: recieves message
 wss.on("connection", function connection(ws) {
   ws.on("message", (message) => {
-    const response: Response = JSON.parse(message.toString());
-    // g. Server Socket: emits message
-    ws.send(JSON.stringify(response));
+    ws.send(message.toString());
   });
 });
 
