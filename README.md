99b3a3cd7497522e6391d9fbc1b4d62ebe056458
leon-do <leondo123@gmail.com>
2021-12-03 23:32:39 -0500

    socket.io

--- README.md
+++ README.md
@@ -18,16 +18,10 @@ c. Desktop Game: deeps like to browser wallet (index.html): https://chainsafe.gi
 
 d. Browser Wallet: Metamask popus up. Alice signs tx gets txHash: 0xtxHash
 
-e. Browser Wallet: sends {id: 0xAlice, receipt: 0xtxHash} to socket
+e. Browser Wallet: sends {user: 0xAlice, receipt: 0xtxHash} to socket
 
-f. Server Socket: recieves message
+f. Server Socket: recieves message and emits
 
-g. Server Socket: emits message
+g. Browser Wallet: Redirect Alice back to game
 
-h. Browser Wallet: Redirect Alice back to game
-
-i. Desktop Game: recieves response and display txHash
-
-## Notes
-
-https://medium.com/unity-nodejs/websocket-client-server-unity-nodejs-e33604c6a006
+h. Desktop Game: recieves response and display txHash
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
-import { WebSocket } from "ws";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const wss = new WebSocket.Server({ port: 3000 });
+interface Transaction {
+  user: string;
+  receipt: string;
+}
 
-// f. Server Socket: recieves message
-wss.on("connection", function connection(ws) {
-  ws.on("message", (message) => {
-    ws.send(message.toString());
+// set up server
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// listen to connection
+io.on("connection", (socket) => {
+  // emit ok every interval
+  setInterval(() => {
+    socket.emit("healthcheck", "OK");
+  }, 3000);
+
+  // gets info from wallet (index.html)
+  socket.on("transaction", (transaction: Transaction) => {
+    const { user, receipt} = transaction;
+    // send transaction receipt to id
+    socket.emit(user, receipt);
   });
 });
 
-wss.on("listening", () => {
-  console.log(3000);
+// start server
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(PORT);
 });
